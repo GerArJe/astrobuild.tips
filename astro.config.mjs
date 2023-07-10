8efd2a0766c4f399a357cdc2e2660cfc0ae9fcc8
German Arevalo J <germanarevalojerez@gmail.com>
2023-07-09 20:27:46 -0500

    sidemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrobuild.tips',
-  integrations: [tailwind(), mdx(), react(), vue(), svelte()]
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
 });
