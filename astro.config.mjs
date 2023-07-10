8c30f04c32ed34048d978acb6959ad737ef21cf4
German Arevalo J <germanarevalojerez@gmail.com>
2023-07-09 20:16:34 -0500

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astrobuild.tips',
   integrations: [tailwind(), mdx(), react(), vue(), svelte()]
 });
